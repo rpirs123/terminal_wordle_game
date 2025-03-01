62e92e82f41e114b32316cad931f7879ed6b1931
Robert Pirs <robertpirs9@gmail.com>
2025-03-01 18:04:03 +0200

    added functionality for randomly choosing the correct word

--- src/main/java/org/example/Word.java
+++ src/main/java/org/example/Word.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Word {
     String correctWord;
 
-    void selectWord(String[] arr){
+    void selectWord(ArrayList<String> arr){
 
-        int index = (int)Math.random() * (arr.length + 1) ;
-        this.correctWord = arr[index];
+        int index = (int)(Math.random() * arr.size());
+        System.out.println("index "+ index);
+        System.out.println("selected word:  "+ arr.get(index));
+        this.correctWord = arr.get(index);
     }
 }
