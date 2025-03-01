e483297f2f99a532035bd4d20afc3daa52f558c0
Robert Pirs <robertpirs9@gmail.com>
2025-03-02 01:58:11 +0200

    added logic to check every letter, added logic and class to print colored letters

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,25 +4,48 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Scanner scanner = new Scanner(System.in);
         Player p = new Player();
         Word w = new Word();
+        String userGuess;
         System.out.println(p.attempts);
         ArrayList<String> words = new ArrayList<String>();
 
         JsonNode data = JsonFileReader.readJsonFromFile("words.json").get("words");
-        if(data.isArray()) {
-            for( JsonNode word : data) {
+        if (data.isArray()) {
+            for (JsonNode word : data) {
                 words.add(word.asText());
             }
         }
-        System.out.println(words);
         w.selectWord(words);
         System.out.println(w.correctWord);
+
+        do {
+            if(p.attempts > 0){
+                for(int i =0; i < p.attempts; i++){
+                    //System.out.println(p.guesses[i]);
+                    w.checkCharacters(p.guesses[i]);
+                    System.out.println();
+                }
+            }
+            int linesToPrint = 5 - p.attempts;
+            for(int i = 0; i < linesToPrint; i++){
+                System.out.println("_"+"_"+"_"+"_"+"_");
+            }
+            userGuess = scanner.nextLine();
+            p.addAttempt();
+            p.addGuess(userGuess);
+        } while (!userGuess.equals(w.correctWord) && p.attempts < 5);
+
+        if(userGuess.equals(w.correctWord)){
+            System.out.println("Congratulations, you won the game in "+p.attempts+ " "+ "tries!");
+        }else{
+            System.out.println("Game Over! You lost!");
+        }
 
 
     }
--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -2,8 +2,15 @@ package org.example;
 
 public class Player {
     int attempts;
+    String[] guesses = {"","","","",""};
 
     Player(){
         attempts = 0;
+    }
+    void addAttempt(){
+        this.attempts +=1;
+    }
+    void addGuess(String guess){
+        this.guesses[this.attempts-1] = guess;
     }
 }
